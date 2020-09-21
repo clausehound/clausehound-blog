55c863a7ff9003f77bdfe5a0b6d2e75dea38b0a2
gizemocak <gizem_ocak@outlook.com>
2020-09-21 14:52:16 -0400

    added unmount function to remove added events and element

--- src/components/hubspotForm.ts
+++ src/components/hubspotForm.ts
@@ -19,33 +19,45 @@ declare var window: Window & typeof globalThis & { hbspt: any };
 const HubspotForm: FC<Props> = ({ title }) => {
   const classes = useStyles();
 
+  const createForm = () => {
+    if (window.hbspt) {
+      window.hbspt.forms.create({
+        portalId: "5646626",
+        formId: "5c64138a-f472-4a76-a705-4e1207408323",
+        target: "#hubspotForm",
+      });
+    }
+  };
+
+  const trackEvent = (event: any) => {
+    if (
+      event.data.type === "hsFormCallback" &&
+      event.data.eventName === "onFormSubmit"
+    ) {
+      trackCustomEvent({
+        category: "Newsletter Signup",
+        action: title,
+        label: event.data.data[0].value,
+      });
+    }
+  };
+
+  const addCreateFormEvent = () => {
+    window.addEventListener("message", trackEvent);
+    createForm();
+  };
+
   useEffect(() => {
     const script = document.createElement("script");
     script.src = "https://js.hsforms.net/forms/v2.js";
     document.body.appendChild(script);
+    script.addEventListener("load", addCreateFormEvent);
 
-    script.addEventListener("load", () => {
-      window.addEventListener("message", event => {
-        if (
-          event.data.type === "hsFormCallback" &&
-          event.data.eventName === "onFormSubmit"
-        ) {
-          trackCustomEvent({
-            category: "Newsletter Signup",
-            action: title,
-            label: event.data.data[0].value,
-          });
-        }
-      });
-
-      if (window.hbspt) {
-        window.hbspt.forms.create({
-          portalId: "5646626",
-          formId: "5c64138a-f472-4a76-a705-4e1207408323",
-          target: "#hubspotForm",
-        });
-      }
-    });
+    return () => {
+      window.removeEventListener("message", trackEvent);
+      script.removeEventListener("load", addCreateFormEvent);
+      if (script.parentNode) script.parentNode.removeChild(script);
+    };
   }, []);
 
   return h(
